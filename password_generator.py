a6267cef12e6b4820d57b785a19dc6bee57b8e76
umbertonesta-stack <umberto.nesta@edu-its.it>
2026-04-17 16:22:34 +0200

    primo commit con funzioni che restituiscono maiuscole, minuscole, cifre e simboli

--- password_generator.py
+++ password_generator.py
@@ -13,11 +13,46 @@ def generate_password(
     
     password = ""
 
-    
+
+
+    # funzioni separate per generare lettere maiuscole, minuscole, numeri e simboli
+    def gen_upper(n) -> str:
+        stringa = ""
+        for i in range(n):
+            stringa += secrets.choice(string.ascii_uppercase)
+        return stringa
+
+    def gen_lower(n) -> str:
+        stringa = ""
+        for i in range(n):
+            stringa += secrets.choice(string.ascii_lowercase)
+        return stringa
+
+    def gen_digits(n) -> str:
+        stringa = ""
+        for i in range(n):
+            stringa += secrets.choice(string.digits)
+        return str(stringa)
+
+    def gen_symbols(n) -> str:
+        stringa = ""
+        for i in range(n):
+            stringa += secrets.choice(string.punctuation)
+        return str(stringa)
 
 
 
-   
+
+
+
+
+
+    return print(gen_upper(1) + gen_lower(1) + gen_digits(1) + gen_symbols(1))
+
+
+    # print(secrets.token_urlsafe(length)) # torna una stringa casuale di lunghezza lenght, composta da lettere, numeri e simboli.
+
+    # print(secrets.choice(string.ascii_letters + string.digits))
     # return password
 
 

3d89e777b1fda35b6831ad01c64fc93bcbd43798
umbertonesta-stack <umberto.nesta@edu-its.it>
2026-04-17 17:01:40 +0200

    terzo commit: funzione genera correttamente una stringa con simboli, numeri e lettere (maiuscole o minuscole). inoltre è presente una verifica alla fien

--- password_generator.py
+++ password_generator.py
@@ -10,33 +10,48 @@ def generate_password(
     use_symbols: bool = True,
     avoid_ambiguous: bool = False,
 )-> str: 
-    
-    #per la lenght data come parametro, chiamare una delle funzioni e pushare il valore dentro la variabile password
-    password = ""
         
+    gen_upper_check = False
+    gen_lower_check = False
+    gen_digits_check = False
+    gen_symbols_check = False    
+
+    # se tutti i flag dei set sono False, o se length è minore del numero di set abilitati, solleva ValueError con un messaggio parlante
+    if not (use_upper or use_lower or use_digits or use_symbols):
+        raise ValueError("Almeno uno dei set di caratteri deve essere abilitato.")
+
+
     # funzioni separate per generare lettere maiuscole, minuscole, numeri e simboli
     def gen_upper(n: int = 1) -> str:
         stringa = ""
         for i in range(n):
             stringa += secrets.choice(string.ascii_uppercase)
+        nonlocal gen_upper_check
+        gen_upper_check = True
         return stringa
 
     def gen_lower(n: int = 1) -> str:
         stringa = ""
         for i in range(n):
             stringa += secrets.choice(string.ascii_lowercase)
+        nonlocal gen_lower_check
+        gen_lower_check = True
         return stringa
 
     def gen_digits(n: int = 1) -> str:
         stringa = ""
         for i in range(n):
             stringa += secrets.choice(string.digits)
+        nonlocal gen_digits_check
+        gen_digits_check = True
         return str(stringa)
 
     def gen_symbols(n: int = 1) -> str:
         stringa = ""
         for i in range(n):
             stringa += secrets.choice(string.punctuation)
+        nonlocal gen_symbols_check  
+        gen_symbols_check = True
         return str(stringa)
 
 
@@ -63,11 +78,19 @@ def generate_password(
     # return print(gen_upper(1) + gen_lower(1) + gen_digits(1) + gen_symbols(1))
     # print(secrets.token_urlsafe(length)) # torna una stringa casuale di lunghezza lenght, composta da lettere, numeri e simboli.
     # print(secrets.choice(string.ascii_letters + string.digits))
-    return print (array_passw)
 
 
-generate_password()
 
+    if (gen_upper_check == False and use_upper == True) or (gen_lower_check == False and use_lower == True) or (gen_digits_check == False and use_digits == True) or (gen_symbols_check == False and use_symbols == True):
+        raise ValueError("la password non è stata generata correttamente... riprova.")
+        
+
+
+    password = ''.join(array_passw)
+    return print (password)
 
 
 print("Generating password...")
+generate_password()
+
+

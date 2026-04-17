aebece26a0603e4e1e462bd43b90a85eb17608c3
umbertonesta-stack <umberto.nesta@edu-its.it>
2026-04-17 16:19:51 +0200

    setup funzione iniziale con i parametri dati da indicazione

---
+++ password_generator.py
@@ -0,0 +1,28 @@
+import secrets
+import string
+
+
+def generate_password(
+    length: int = 19,
+    use_upper: bool = True,
+    use_lower: bool = True,
+    use_digits: bool = True,
+    use_symbols: bool = True,
+    avoid_ambiguous: bool = False,
+)-> str: 
+    
+    password = ""
+
+    
+
+
+
+   
+    # return password
+
+
+generate_password()
+
+
+
+print("Generating password...")
